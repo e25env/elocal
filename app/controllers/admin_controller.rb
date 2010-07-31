d0b483a9fc4ffbcf00ed659fd53e37e2d3698e68
Songrit Leemakdej <songrit@gmail.com>
2010-08-01 02:56:59 +0700

    edit org

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,10 @@
 class AdminController < ApplicationController
   before_filter :admin_action, :except=>[:daily_housekeeping, :hourly_housekeeping]
 
+  def update_org
+    org = Org.update 1,$xvars[:edit][:org]
+    org.id
+  end
   def users
     @users= User.paginate :page=>params[:page], :per_page=>25,
       :order=>:login
@@ -16,6 +20,8 @@ class AdminController < ApplicationController
   def destroy_gma
     GmaXmain.destroy_all
     GmaRunseq.destroy_all
+    GmaDoc.destroy_all
+    GmaSearch.destroy_all
     redirect_to "/admin/stat"
   end
 
