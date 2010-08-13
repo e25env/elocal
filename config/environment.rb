8ec30929d85f4dee34e6af59e89a55b06d242a1f
Songrit Leemakdej <songrit@gmail.com>
2010-08-13 13:35:09 +0700

    whenever gem

--- config/environment.rb
+++ config/environment.rb
@@ -39,6 +39,8 @@ Rails::Initializer.run do |config|
   config.gem 'mongo_mapper'
   config.gem 'bson_ext', :lib=>false
   config.gem 'firewatir'
+  config.gem 'chronic'
+  config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
