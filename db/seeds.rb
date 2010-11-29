942aa0644b9ffdb9322d89e854ead615478eb84f
Songrit Leemakdej <songrit@gmail.com>
2010-11-29 22:09:47 +0700

    fix add village head

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+unless Org.exists?
+  puts "create sample Org"
+  Org.create! :otype=> "องค์การบริหารส่วนตำบล",
+    :otype_abbrev=> "อบต.",
+    :name=> "บางตะไนย์",
+    :address=> "43/8 หมู่ที่ 5",
+    :district=> "ปากเกร็ด",
+    :province=> "นนทบุรี ",
+    :phone=> "0-2501-7321-2",
+    :villages=> 5
+end
+
+['ม. 6','ปริญญาตรี','ปริญญาโท','ปริญญาเอก'].each do |e|
+  EducationLevel.find_or_create_by_name(e)
+end
+
+unless Section.exists?
+   puts "create sections"
+   Section.create :code=>"01", :name => "สำนักงานปลัด อบต.", :doc_code=>"\340\270\233"
+   Section.create :code=>"02", :name => "ส่วนการคลัง", :doc_code=>"\340\270\204"
+   Section.create :code=>"03", :name => "ส่วนโยธา", :doc_code=>"\340\270\242"
+   Section.create :code=>"04", :name => "ส่วนสาธารณสุขและสิ่งแวดล้อม", :doc_code=>"\340\270\252"
+end
