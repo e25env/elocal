9720cbbe8d986c2922d10ebd269494e24304eb9c
Songrit Leemakdej <songrit@gmail.com>
2010-12-14 12:10:51 +0700

    refactor senior to use Person

--- app/models/senior.rb
+++ app/models/senior.rb
@@ -1,18 +1,19 @@
 class Senior < ActiveRecord::Base
+  belongs_to :person
+  belongs_to :address
   named_scope :seniors, :conditions=>{:underprivileged=>1}
   named_scope :handicaps, :conditions=>{:underprivileged=>2}
   named_scope :aids, :conditions=>{:underprivileged=>3}
   validate :no_duplicate
 
-  def nid=(val)
-    self[:nid]= val.gsub(/[\s-]/,'')
-  end
+#  def nid=(val)
+#    self[:nid]= val.gsub(/[\s-]/,'')
+#  end
   def full_name
-    "#{title}#{fname} #{lname}"
+    "#{person.title}#{person.fname} #{person.lname}"
   end
-
   def no_duplicate
     errors.add(:nid, "ขออภัย มีบุคคลนี้ในระบบอยู่แล้ว") if
-      Senior.exists?(:nid=>nid, :underprivileged=>underprivileged)
+      Senior.exists?(:person_id=>person_id, :underprivileged=>underprivileged)
   end
 end
