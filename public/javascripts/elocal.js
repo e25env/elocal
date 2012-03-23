112d68248010903e2ca053822490d15046dcdf83
songrit <songrit@gmail.com>
2012-03-23 07:49:36 +0700

    hide body, wait for css then show() in elocal.js

--- public/javascripts/elocal.js
+++ public/javascripts/elocal.js
@@ -1,4 +1,5 @@
 $(document).bind("mobileinit", function(){
+  $('body').show();
   $.extend(  $.mobile , {
     loadingMessage: 'กรุณารอสักครู่',
     pageLoadErrorMessage: "ขออภัย ไม่สามารถดำเนินการได้"
