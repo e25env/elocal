8d8760cd61ce18bc1d8bba50d195fb35247eafa2
songrit <songrit@gmail.com>
2012-04-27 16:29:27 +0700

    sample for presentation at princess final

---
+++ app/controllers/mod_controller.rb
@@ -0,0 +1,2 @@
+class ModController < ApplicationController
+end
---
+++ spec/controllers/mod_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ModController do
+
+  #Delete this example and add some real ones
+  it "should use ModController" do
+    controller.should be_an_instance_of(ModController)
+  end
+
+end
